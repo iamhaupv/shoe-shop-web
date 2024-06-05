66855e00bf843d2b5b627f97560d1377e5ff8d6b
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-05 15:32:10 +0700

    sign up

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import logo from "../assets/logo.png"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import Banner from "../components/Banner"
 const Signin = () => {
   const [phone, setPhone] = useState("")
@@ -9,28 +9,37 @@ const Signin = () => {
     setPhone(event.target.value)
     console.log(phone)
   }
-  const handlePassword = (event) =>{
+  const handlePassword = (event) => {
     setPassword(event.target.value)
   }
-  const handleSubmit = () =>{
+  const handleSubmit = () => {
     const newUser = [phone, password]
     console.log(newUser)
   }
   return (
     <div className="h-screen flex">
+      {/* left */}
       <div className="w-1/2 flex-col justify-center mt-24">
+        {/* logo */}
         <div className="flex justify-center ">
           <img src={logo} alt="logo" width={200} height={200} />
         </div>
+        {/* title */}
         <div className="flex justify-center">
           <p className="text-3xl">Sign in to your account</p>
         </div>
-        <div className="mt-3">
+        {/* form */}
+        <form className="mt-3">
           <div className="w-500 mx-auto">
             <label className="block">Phone number</label>
             {/* phone number */}
             <div>
-              <input className="border border-2 border-solid border-black w-full" type="text" name="phone" value={phone} onChange={handlePhone} />
+              {/* input phone number */}
+              <input className="border border-2 border-solid border-black w-full"
+                type="text"
+                name="phone"
+                value={phone}
+                onChange={handlePhone} />
             </div>
           </div>
           <div className="w-500 mx-auto">
@@ -40,26 +49,29 @@ const Signin = () => {
                 <a className="text-blue-500" href="https://fb.com">Forgot password?</a>
               </div>
             </div>
+            {/* password */}
             <div className="flex justify-center">
-              {/* password */}
+              {/* input password */}
               <input className="border border-2 border-solid border-black w-full"
                 value={password}
                 onChange={handlePassword}
               />
             </div>
           </div>
+          {/* button */}
           <div className="w-500 mx-auto mt-2">
             <button className="bg-blue-300 w-full border-2 border-solid border border-black"
               onClick={handleSubmit}
             >Sign in</button>
           </div>
-        </div>
+        </form>
         {/* link sign up */}
         <div className="flex justify-center">
           <Link to="/signup" className="text-blue-500">Create account</Link>
         </div>
       </div>
-      <Banner/>
+      {/* right */}
+      <Banner />
     </div>
   )
 }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,14 +1,69 @@
 import Banner from "../components/Banner"
 import logo from "../assets/logo.png"
-const Signup = () =>{
+import { useState } from "react"
+const Signup = () => {
+    const [phone, setPhone] = useState("")
+    const [password, setPassword] = useState("")
+    const handlePhone = (event) => {
+        setPhone(event.target.value)
+        console.log(phone)
+    }
+    const handlePassword = (event) => {
+        setPassword(event.target.value)
+    }
+    const handleSubmit = () => {
+        const newUser = [phone, password]
+        console.log(newUser)
+    }
     return (
         <div className="h-screen flex">
-            <div className="w-1/2 flex-col">
-                <div>
-                <img src={logo} alt="logo" width={200} height={200} />
+            {/* left */}
+            <div className="w-1/2 flex-col mt-24">
+                {/* logo */}
+                <div className="flex justify-center">
+                    <img src={logo} alt="logo" width={200} height={200} />
                 </div>
+                {/* title */}
+                <div className="flex justify-center">
+                    <p className="text-3xl">Welcom to PHV-SHOP</p>
+                </div>
+                {/* form */}
+                <form className="mt-3">
+                    {/* phone number */}
+                    <div className="w-500 mx-auto">
+                        <div>
+                            <label className="block">Phone number</label>
+                        </div>
+                        {/* input phone number */}
+                        <div>
+                            <input className="border border-2 border-solid border-black w-full"
+                                type="text"
+                                name="phone"
+                                value={phone}
+                                onChange={handlePhone} />
+                        </div>
+                    </div>
+                    {/* password */}
+                    <div className="w-500 mx-auto">
+                        <div><label>Password</label></div>
+                        <div className="flex justify-center">
+                            {/* input password */}
+                            <input className="border border-2 border-solid border-black w-full"
+                                value={password}
+                                onChange={handlePassword}
+                            />
+                        </div>
+                    </div>
+                    {/* button */}
+                    <div className="w-500 mx-auto mt-2">
+                        <button className="bg-blue-300 w-full border-2 border-solid border border-black"
+                            onClick={handleSubmit}
+                        >Sign up</button>
+                    </div>
+                </form>
             </div>
-            <Banner/>
+            {/* right */}
+            <Banner />
         </div>
     )
 }
