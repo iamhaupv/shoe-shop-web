3ce86383272f9c7430a219e9efe447b1d4652ee6
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-08 22:40:31 +0700

    add product

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,5 +1,48 @@
+import { useNavigate } from "react-router-dom";
+
 const AddProduct = () => {
-  return <div>Hello</div>;
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    navigate("/admin-manager-product")
+  }
+  return (
+    <div>
+      <div>
+        <table>
+          <tr>
+            <td>Name</td>
+            {/* name */}
+            <td>
+              <input
+                type="text"
+                className="border border-2 border-solid border-black"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Quantity</td>
+            {/* quantity */}
+            <td>
+              <input
+                type="text"
+                className="border border-2 border-solid border-black"
+              />
+            </td>
+          </tr>
+          <tr>
+            {/* submit */}
+            <td>
+              <input
+                type="submit"
+                className="border border-2 border-solid border-black bg-green-500"
+                onClick={handleSubmit}
+              />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AddProduct;
