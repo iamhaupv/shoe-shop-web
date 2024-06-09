c696dd16eeb48dd92271dee2e94c380d5d33b9b2
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-09 13:08:54 +0700

    show password logn in admin

--- src/pages/LoginAdmin.js
+++ src/pages/LoginAdmin.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 const LogninAdmin = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [password, setPassword] = useState("");
