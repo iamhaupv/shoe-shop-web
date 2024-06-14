453da110efa50ec31072966ae433196a355ee4ab
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-14 16:27:52 +0700

    update host admin/LoginAdminService

--- src/services/admin/LoginAdminService.js
+++ src/services/admin/LoginAdminService.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import GlobalHost from "../../GlobalHost";
 //
 const LoginAdminService = async (phoneNumber, password) => {
   try {
-    const response = await axios.post("http://localhost:3000/wp-admin/login", {
+    const response = await axios.post(`${GlobalHost.host_admin}/login`, {
       phoneNumber,
       password,
     });
