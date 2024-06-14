6b87f9e5c1a7974df7a58baab9e4f9f2d53600d4
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-14 16:22:32 +0700

    update LoginService

--- src/services/user/LoginService.js
+++ src/services/user/LoginService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+import GlobalHost from "../../GlobalHost";
 const LoginService = async (phoneNumber, password) => {
   try {
-    const reponse = await axios.post("http://localhost:3000/users/login", {
+    const reponse = await axios.post(`${GlobalHost.host_user}/login`, {
       phoneNumber,
       password,
     });
