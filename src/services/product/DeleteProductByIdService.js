724b5f4b4ca8cf814eaac5d0cd7554de00da656f
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-14 16:17:48 +0700

    update host DeleteProductByIdService

--- src/services/product/DeleteProductByIdService.js
+++ src/services/product/DeleteProductByIdService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-
+import GlobalHost from "../../GlobalHost";
 const DeleteProductByIdService = async (token, _id) => {
   try {
     const response = await axios.delete(
-      `http://localhost:3000/products/delete-product-by-id/${_id}`,
+      `${GlobalHost.host_product}/delete-product-by-id/${_id}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
