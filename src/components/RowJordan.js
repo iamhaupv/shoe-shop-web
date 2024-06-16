952f0a87f414b72bbf6a0de774ac187c9463773f
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-16 17:07:50 +0700

    view 4 break

--- src/components/RowJordan.js
+++ src/components/RowJordan.js
@@ -1,13 +1,11 @@
+import React, { useEffect, useState } from "react";
 import FrameProduct from "./FrameProduct";
 import jordan1 from "../assets/jordan-1.jpeg";
-import jordan2 from "../assets/jordan-2.jpg";
-import jordan3 from "../assets/jordan-3.jpg";
-import jordan4 from "../assets/jordan-4.jpg";
-import { useEffect, useState } from "react";
 import FindAllProductService from "../services/product/FindAllProductService";
+
 const RowJordan = () => {
-  const [name, setName] = useState("");
-  const [shoes, setShoes] = useState([])
+  const [shoes, setShoes] = useState([]);
+
   useEffect(() => {
     const fetchAllProduct = async () => {
       try {
@@ -16,24 +14,18 @@ const RowJordan = () => {
           throw new Error("Token invalid!");
         }
         const response = await FindAllProductService(token);
-        setShoes(response.data)
+        setShoes(response.data);
       } catch (error) {
-        throw new Error(error);
+        console.error(error);
       }
     };
     fetchAllProduct();
   }, []);
+
   return (
-    <div className="flex w-full mx-auto px-[50px] mt-4">
-      {/* <FrameProduct imageName={jordan1} />
-      <div className="w-[8px]"></div>
-      <FrameProduct imageName={jordan2} />
-      <div className="w-[8px]"></div>
-      <FrameProduct imageName={jordan3} />
-      <div className="w-[8px]"></div>
-      <FrameProduct imageName={jordan4} /> */}
+    <div className="flex flex-wrap w-full mx-auto px-[50px] mt-4 ">
       {shoes.map((shoe, index) => (
-        <FrameProduct name={shoe.name} imageName={jordan1} />
+        <FrameProduct key={index} name={shoe.name} imageName={jordan1} />
       ))}
     </div>
   );
