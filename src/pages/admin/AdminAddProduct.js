10c6d9d65ae519f1f9f3bd0f12bdb2cb86b8d569
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-24 19:32:42 +0700

    regex name, quantity, category

--- src/pages/admin/AdminAddProduct.js
+++ src/pages/admin/AdminAddProduct.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import AddProductService from "../../services/product/AddProductService";
 import FindAllCategoriesService from "../../services/category/FindAllCategoriesService";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { useDropzone } from "react-dropzone";
 
 const AdminAddProduct = () => {
@@ -17,8 +17,14 @@ const AdminAddProduct = () => {
   const [size, setSize] = useState("36");
   const [images, setImages] = useState([]);
   const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState(""); // State for selected category
-  const navigate = useNavigate();
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [nameError, setNameError] = useState(false); // error name
+  const [iconName, setIconName] = useState(false); // icon name
+  const [quantityError, setQuantityError] = useState(false); // error quantity
+  const [iconQuantity, setIconQuantity] = useState(false); // icon quantity
+  const [categoryError, setCategoryError] = useState(false); // category error
+  const [iconCategory, setIconCategory] = useState(false); // icon category
+  const navigate = useNavigate(); // navigate
 
   // Fetch categories from API on component mount
   useEffect(() => {
@@ -46,7 +52,7 @@ const AdminAddProduct = () => {
       if (!token) {
         throw new Error("Token invalid!");
       }
-
+      // form
       const formData = new FormData();
       formData.append("name", name);
       formData.append("quantity", quantity);
@@ -95,6 +101,60 @@ const AdminAddProduct = () => {
     onDrop,
   });
 
+  // regex name
+  const regexName = async (value) => {
+    const regex = /^[a-zA-Z\s]+$/;
+    if (value === "" || !regex.test(value)) {
+      setNameError(true);
+    } else {
+      setNameError(false);
+    }
+  };
+  // handle change name
+  const handleChangeName = (e) => {
+    regexName(e.target.value.trim());
+    setName(e.target.value);
+    setIconName(false);
+  };
+  // handle blur name
+  const handleBlurName = (e) => {
+    regexName(e.target.value);
+    setIconName(true);
+  };
+  // regex quantity
+  const regexQuantity = async (value) => {
+    const regex = /^[1-9][0-9]*$/;
+    if (value === "" || !regex.test(value)) {
+      setQuantityError(true);
+    } else {
+      setQuantityError(false);
+    }
+  };
+  // handle change quantity
+  const handleChangeQuantity = (e) => {
+    regexQuantity(e.target.value.trim());
+    setQuantity(e.target.value);
+    setIconQuantity(false);
+  };
+  // handle blur quantity
+  const handleBlurQuantity = (e) => {
+    regexQuantity(e.target.value);
+    setIconQuantity(true);
+  };
+  // handle change category
+  const handleChangeCategory = (e) => {
+    setSelectedCategory(e.target.value);
+    if (e.target.value === "") {
+      setCategoryError(true);
+    } else {
+      setCategoryError(false);
+    }
+  };
+  // handle blur category
+  const handleBlurCategory = () => {
+    if(selectedCategory === "")
+      setIconCategory(true)
+  };
   return (
     <div className="p-6">
       {/* Return Button */}
@@ -106,9 +166,15 @@ const AdminAddProduct = () => {
           <FontAwesomeIcon icon={faArrowLeft} />
         </button>
       </div>
+      {/* Title */}
+      <div>
+        <h1 className="text-4xl font-bold flex justify-center text-red-400">
+          Form Add Shoes
+        </h1>
+      </div>
       {/* Product Details Form */}
-      <div className="mb-6">
-        <table className="min-w-full border-collapse">
+      <div className="mb-6 mt-6">
+        <table className="w-full border-collapse table-fixed">
           <tbody>
             {/* name */}
             <tr>
@@ -117,9 +183,22 @@ const AdminAddProduct = () => {
                 <input
                   type="text"
                   value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  className="border border-gray-300 rounded-md p-2 w-full"
+                  onBlur={handleBlurName}
+                  onChange={handleChangeName}
+                  className={`border border-gray-300 rounded-md p-2 w-full ${
+                    nameError ? "border-red-500" : ""
+                  }`}
                 />
+              </td>
+              <td>
+                {iconName &&
+                  (nameError ? (
+                    <span className="text-red-500 font-bold">*</span>
+                  ) : (
+                    <span className="text-green-500 font-bold">
+                      <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                  ))}
               </td>
             </tr>
             {/* quantity */}
@@ -129,9 +208,21 @@ const AdminAddProduct = () => {
                 <input
                   type="text"
                   value={quantity}
-                  onChange={(e) => setQuantity(e.target.value)}
-                  className="border border-gray-300 rounded-md p-2 w-full"
+                  onChange={handleChangeQuantity}
+                  onBlur={handleBlurQuantity}
+                  className={`border border-gray-300 rounded-md p-2 w-full 
+                    ${quantityError ? "border-red-500" : ""}`}
                 />
+              </td>
+              <td>
+                {iconQuantity &&
+                  (quantityError ? (
+                    <span className="text-red-500">*</span>
+                  ) : (
+                    <span className="text-green-500">
+                      <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                  ))}
               </td>
             </tr>
             {/* category */}
@@ -139,9 +230,12 @@ const AdminAddProduct = () => {
               <td className="px-4 py-2 border-b">Category</td>
               <td className="px-4 py-2 border-b">
                 <select
-                  value={selectedCategory} // Bind to selectedCategory state
-                  onChange={(e) => setSelectedCategory(e.target.value)} // Update selectedCategory state
-                  className="border border-gray-300 rounded-md p-2 w-full"
+                  value={selectedCategory}
+                  onChange={handleChangeCategory}
+                  onBlur={handleBlurCategory}
+                  className={`border border-gray-300 rounded-md p-2 w-full ${
+                    categoryError ? "border-red-500" : ""
+                  }`}
                 >
                   <option value="">Select a category...</option>
                   {categories.map((category) => (
@@ -150,6 +244,16 @@ const AdminAddProduct = () => {
                     </option>
                   ))}
                 </select>
+              </td>
+              <td>
+                {iconCategory &&
+                  (categoryError ? (
+                    <span className="text-red-500">*</span>
+                  ) : (
+                    <span className="text-green-500">
+                      <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                  ))}
               </td>
             </tr>
             {/* price */}
