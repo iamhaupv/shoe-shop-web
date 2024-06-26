20dff06fb1d7a226a2000c5421baf94bd4dd8b97
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-26 17:46:05 +0700

    update

---
+++ src/services/user/CheckUserExistService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import GlobalHost from "../../GlobalHost";
+
+const CheckUserExistService = async (token, phoneNumber) => {
+  try {
+    const response = await axios.get(
+      `${GlobalHost.host_user}/check-user-exist`,
+      { phoneNumber },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default CheckUserExistService;
