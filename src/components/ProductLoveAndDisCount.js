8222cf9854f5847b3213f4640b8b5318763c6d33
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-07-23 21:36:46 +0700

    ProductLoveAndDiscount

--- src/components/ProductLoveAndDisCount.js
+++ src/components/ProductLoveAndDisCount.js
@@ -64,7 +64,8 @@ export default function ProductLoveAndDisCount() {
                 padding: "8px 0px 0px",
                 position: "absolute",
                 top: "5px",
-                zIndex: "999"
+                zIndex: "999",
+                left: "6px"
               }}
             >
               <div style={{ height: "18px" }}>
@@ -164,76 +165,6 @@ export default function ProductLoveAndDisCount() {
             </div>
           </a>
         ))}
-        {/* <div
-          style={{
-            width: "200px",
-            height: "308px",
-            padding: "5px",
-            border: "1px solid #e5e5e5",
-          }}
-        >
-          <img src={product1} alt="" />
-          <div style={{ width: "188.4px", height: "109.6px", padding: "8px" }}>
-            <div
-              className="flex justify-center items-center"
-              style={{
-                width: "172.4px",
-                height: "40px",
-                color: "#000000CC",
-                margin: "0px 0px 4px",
-              }}
-            >
-              dsfsafsfds
-            </div>
-            <div
-              className="flex justify-center items-center"
-              style={{
-                width: "172.4px",
-                height: "20px",
-                margin: "0px 0px 8px",
-              }}
-            ></div>
-            <div
-              className="flex justify-between items-center"
-              style={{
-                width: "172.4px",
-                height: "20px",
-                margin: "0px 0px 8px",
-              }}
-            >
-              <div>
-                <span style={{ fontSize: "12px" }}>d</span>
-                <span style={{ fontSize: "16px" }}>lkjl</span>
-              </div>
-              <div
-                style={{
-                  width: "71.39px",
-                  height: "16px",
-                  color: "#000000DE",
-                  fontSize: "12px",
-                  margin: "0px 0px 0px 4px",
-                }}
-              >
-                da ban 188
-              </div>
-            </div>
-          </div>
-        </div> */}
-        {/* <div style={{ width: "200px", height: "308px", padding: "5px" }}>
-          <img src={product1} alt="" />
-        </div>
-        <div style={{ width: "200px", height: "308px", padding: "5px" }}>
-          <img src={product1} alt="" />
-        </div>
-        <div style={{ width: "200px", height: "308px", padding: "5px" }}>
-          <img src={product1} alt="" />
-        </div>
-        <div style={{ width: "200px", height: "308px", padding: "5px" }}>
-          <img src={product1} alt="" />
-        </div>
-        <div style={{ width: "200px", height: "308px", padding: "5px" }}>
-          <img src={product1} alt="" />
-        </div> */}
       </div>
     </div>
   );
