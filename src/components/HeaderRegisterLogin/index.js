26b31e2f125c460b4e06b66d90d700eb6703b32c
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-05 00:13:17 +0700

    update style HeaderRegisterLogin

--- src/components/HeaderRegisterLogin/index.js
+++ src/components/HeaderRegisterLogin/index.js
@@ -1,59 +1,38 @@
 import React from "react";
-import logo_h from "../../assets/logo_h.png"
+import logo_h from "../../assets/logo_h.png";
+import "./HeaderRegisterLogin.css";
 export default function HeaderRegisterLogin(props) {
   return (
-    <div
-      className="flex justify-center items-center"
-      style={{
-        width: "100%",
-        height: "84px",
-        background: "#ffffff",
-        fontFamily:
-          "'Helvetica Neue', Helvetica, Arial, '文泉驛正黑', 'WenQuanYi Zen Hei', 'Hiragino Sans GB', '儷黑 Pro', 'LiHei Pro', 'Heiti TC', '微軟正黑體', 'Microsoft JhengHei UI', 'Microsoft JhengHei', sans-serif",
-        lineHeight: 1.2,
-      }}
-    >
-      <div
-        className="flex justify-between items-center"
-        style={{ width: "1200px", height: "32px", margin: "0px 34.4px" }}
-      >
-        <div className="flex justify-center items-center">
-          <a href="fb.com"><div
-            className="flex justify-center items-center"
-            style={{ width: "190px", height: "42px" }}
-          >
-            <div
-              className="flex justify-center items-center"
-              style={{ width: "135px", height: "39.25px", color: "#ee4d2d", margin: "-10px -18px 0px 0px" }}
-            >
-              <span className="flex justify-center items-center" style={{background: "#ffffff"}}>
-               <img src={logo_h} alt="" className="object-cover w-[100%] h-[100%]"/>
-              </span>
-              <span style={{ fontSize: "24px", marginTop: "10px", marginLeft: "5px" }}>auShop</span>
+    // header
+    <div className="flex justify-center items-center header">
+      {/* header__content */}
+      <div className="flex justify-between items-center header__content">
+        {/* header__content__left */}
+        <div className="flex justify-center items-center header__content__left">
+          {/* header_content__left wrap__logo */}
+          <a href="fb.com">
+            <div className="flex justify-center items-center wrap__logo">
+              {/* wrap__logo__child */}
+              <div className="flex justify-center items-center wrap__logo__child">
+                {/* wrap_logo_img */}
+                <div className="flex justify-center items-center">
+                  <img src={logo_h} alt="Logo" className="wrap_logo_img" />
+                </div>
+                {/* wrap__logo__text */}
+                <span className="wrap__logo__text">auShop</span>
+              </div>
             </div>
-          </div></a>
-          <div
-            className="flex justify-center items-center"
-            style={{
-              width: "117.44px",
-              height: "28.8px",
-              color: "#222222",
-              fontSize: "24px",
-            }}
-          >
+          </a>
+          {/* wrap__text */}
+          <div className="flex justify-center items-center wrap__text">
             {props.text}
           </div>
         </div>
+        {/* header__content__right */}
         <div>
           <a
             href="fb.com"
-            style={{
-              width: "110.38px",
-              height: "16.8px",
-              color: "#ee4d2d",
-              fontSize: "14px",
-              margin: "0px 15px 0px 0px",
-            }}
+            className="header__content__right"
           >
             Bạn cần giúp đỡ?
           </a>
