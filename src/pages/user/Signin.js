012c35ee9f66ab51ad46c830c90c5828ab680cbd
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-05 11:57:47 +0700

    update style  Form Login

--- src/pages/user/Signin.js
+++ src/pages/user/Signin.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import FooterRegisterLogin from "../../components/FooterRegisterLogin"
-import FormLogin from "../../components/FormLogin";
+import FormLogin from "../../components/FormLogin/index";
 import HeaderRegisterLogin from '../../components/HeaderRegisterLogin';
 import ContentRegisterLogin from "../../components/ContentRegisterLogin"
 export default function Signin() {
