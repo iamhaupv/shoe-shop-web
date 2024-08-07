d9643e9daead7ca73c1c90454f59271cfa61773a
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-07 17:13:25 +0700

    update Fragment BannerViewHome

--- src/components/BannerViewHome/index.js
+++ src/components/BannerViewHome/index.js
@@ -3,12 +3,17 @@ import BannerVoucher from "../BannerVoucher";
 
 const BannerViewHome = () => {
   return (
-    <div className="flex justify-center px-[50px] mx-auto">
-      <div className="flex items-center justify-between" style={{width: "1200px"}}>
-        <BannerShoe />
-        <BannerVoucher />
+    <>
+      <div className="flex justify-center px-[50px] mx-auto">
+        <div
+          className="flex items-center justify-between"
+          style={{ width: "1200px" }}
+        >
+          <BannerShoe />
+          <BannerVoucher />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
