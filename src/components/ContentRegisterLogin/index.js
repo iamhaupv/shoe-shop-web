a5ba8b312bfe7a41b327560de8631b33f7f1768c
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-07 16:24:20 +0700

    update Fragment ContentRegisterLogin

--- src/components/ContentRegisterLogin/index.js
+++ src/components/ContentRegisterLogin/index.js
@@ -3,22 +3,28 @@ import banner_myshoe from "../../assets/pngegg.png";
 import "./ContentRegisterLogin.css";
 export default function ContentRegisterLogin({ component: Component }) {
   return (
-    // content
-    <div className="flex justify-center items-center content">
-      {/* content__child */}
-      <div className="flex items-center justify-end content__child ">
-        <div className="flex justify-center items-center">
-          {/* banner */}
-          <div className="banner">
-            {/* banner__child */}
-            <div className="flex justify-center items-center banner__child">
-              {/* banner__child__img */}
-              <img src={banner_myshoe} alt="Banner" className="banner__child__img" />
+    <>
+      {/* content */}
+      <div className="flex justify-center items-center content">
+        {/* content__child */}
+        <div className="flex items-center justify-end content__child ">
+          <div className="flex justify-center items-center">
+            {/* banner */}
+            <div className="banner">
+              {/* banner__child */}
+              <div className="flex justify-center items-center banner__child">
+                {/* banner__child__img */}
+                <img
+                  src={banner_myshoe}
+                  alt="Banner"
+                  className="banner__child__img"
+                />
+              </div>
             </div>
           </div>
+          {Component && <Component />}
         </div>
-        {Component && <Component />}
       </div>
-    </div>
+    </>
   );
 }
