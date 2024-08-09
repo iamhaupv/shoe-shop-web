d1e0a94243700e434e0bc817b8c41e289825eb3c
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-09 15:41:01 +0700

    update style ProductMall

--- src/components/ProductMall/index.js
+++ src/components/ProductMall/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import mall from "../../assets/mall.png";
 import FindAllProductShopMallService from "../../services/product/FindAllProductShopMallService";
+import "./ProductMall.css";
 export default function ProductMall() {
   const [products, setProducts] = useState([]);
   useEffect(() => {
@@ -12,99 +13,36 @@ export default function ProductMall() {
   }, []);
   return (
     <>
-      {products.map((product, index) => (
-        <a href="fb.com" style={{ position: "relative" }}>
-          {/* lovely */}
-          <div
-            style={{
-              width: "190.06px",
-              height: "26px",
-              margin: "0px 0px 0px -3px",
-              padding: "8px 0px 0px",
-              position: "absolute",
-              top: "5px",
-              zIndex: "999",
-              left: "6px",
-            }}
-          >
-            <div style={{ height: "18px" }}>
-              <img
-                src={mall}
-                style={{ width: "auto", height: "100%" }}
-                className="object-cover"
-                alt=""
-              />
+      {/* content__product */}
+      {products.map((product) => (
+        <a href="fb.com" key={product._id} className="content__product">
+          {/* logo__mall */}
+          <div className="logo__mall">
+            {/* logo__mall__child */}
+            <div className="logo__mall__child">
+              <img src={mall} alt="logo__mall" />
             </div>
           </div>
-          <div
-            style={{
-              width: "200px",
-              height: "308px",
-              padding: "5px",
-            }}
-          >
-            <div
-              style={{
-                width: "188.4px",
-                height: "298px",
-                border: "1px solid #e5e5e5",
-                position: "relative",
-              }}
-            >
-              <img
-                style={{ width: "188.4px", height: "188.4px" }}
-                src={product.images[0]}
-                alt=""
-              />
-              <div
-                style={{
-                  width: "188.4px",
-                  height: "109.6px",
-                  padding: "8px",
-                }}
-              >
-                <div
-                  className="flex justify-center items-center"
-                  style={{
-                    width: "172.4px",
-                    height: "40px",
-                    color: "#000000CC",
-                    margin: "0px 0px 4px",
-                  }}
-                >
-                  {product.name}
-                </div>
-                <div
-                  className="flex justify-center items-center"
-                  style={{
-                    width: "172.4px",
-                    height: "20px",
-                    margin: "0px 0px 8px",
-                  }}
-                ></div>
-                <div
-                  className="flex justify-between items-center"
-                  style={{
-                    width: "172.4px",
-                    height: "20px",
-                    margin: "0px 0px 8px",
-                  }}
-                >
-                  <div style={{ color: "#ee4d2d" }}>
-                    <span style={{ fontSize: "12px" }}>₫</span>
-                    <span style={{ fontSize: "16px" }}>{product.price}</span>
+          {/* wrap__product */}
+          <div className="wrap__product">
+            {/* wrap__detail */}
+            <div className="wrap__details">
+              <img src={product.images[0]} alt={product.name} />
+              {/* des */}
+              <div className="des">
+                {/* des__name */}
+                <div className="des__name">{product.name}</div>
+                {/* des__active */}
+                <div className="des__active"></div>
+                {/* wrap__price__sold */}
+                <div className="wrap__price__sold">
+                  {/* child__price__sold */}
+                  <div className="child__price__sold">
+                    <span>₫</span>
+                    <span>{product.price}</span>
                   </div>
-                  <div
-                    style={{
-                      width: "71.39px",
-                      height: "16px",
-                      color: "#000000DE",
-                      fontSize: "12px",
-                      margin: "0px 0px 0px 4px",
-                    }}
-                  >
-                    da ban 188
-                  </div>
+                  {/* sold */}
+                  <div className="sold">da ban 188</div>
                 </div>
               </div>
             </div>
